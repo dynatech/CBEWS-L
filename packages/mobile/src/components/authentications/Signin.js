b38ea2acad3c706370b78284979bf906d4ab3731
John Geliberte <gelibertejohn@gmail.com>
2020-05-15 21:18:00 +0800

    + Added formik implementation

--- packages/mobile/src/components/authentications/Signin.js
+++ packages/mobile/src/components/authentications/Signin.js
@@ -5,6 +5,7 @@ import { ContainerStyle } from '../../styles/container_style';
 import { ImageStyle } from '../../styles/image_style';
 import { InputStyle } from '../../styles/input_style';
 import { LabelStyle } from '../../styles/label_style';
+import { Formik } from 'formik';
 
 function Signin(props) {
     const navigator = props.navigation;
@@ -21,20 +22,34 @@ function Signin(props) {
                         <Image style={ImageStyle.seal} source={require('../../assets/leon_seal.png')}></Image>
                         <Image style={ImageStyle.seal} source={require('../../assets/umi_seal.png')}></Image>
                     </View>
-                    <View style={ContainerStyle.login_content}>
-                        <Text style={[LabelStyle.large_label, LabelStyle.default, InputStyle.white]}>Community Based Early Warning System for Landslides</Text>
-                        <TextInput style={[InputStyle.large, InputStyle.default, InputStyle.white]} placeholder="Username" placeholderTextColor="#fff" />
-                        <TextInput style={[InputStyle.large, InputStyle.default, InputStyle.white]} secureTextEntry={true} placeholder="Password" placeholderTextColor="#fff" />
-                        <Text style={[LabelStyle.brand]} onPress={() => {
-                            navigator.navigate('ForgotPassword');
-                        }}>Forgot password?</Text>
-                        <Text style={[LabelStyle.medium_label, LabelStyle.brand]} onPress={() => {
-                            navigator.navigate('Signup');
-                        }}>Create account</Text>
-                        <TouchableOpacity style={ButtonStyle.large} onPress={() => { }}>
-                            <Text style={ButtonStyle.large_text}>Sign in</Text>
-                        </TouchableOpacity>
-                    </View>
+                    <Formik
+                        initialValues={{username: '', password: ''}}
+                        onSubmit={values => console.log(values)}
+                        >
+                        {({ handleChange, handleBlur, handleSubmit, values }) => (
+                            <View style={ContainerStyle.login_content}>
+                                <Text style={[LabelStyle.large_label, LabelStyle.default, InputStyle.white]}>Community Based Early Warning System for Landslides</Text>
+                                <TextInput style={[InputStyle.large, InputStyle.default, InputStyle.white]} 
+                                                values={values.username} 
+                                                placeholder="Username" 
+                                                placeholderTextColor="#fff"
+                                                onChangeText={handleChange('username')} />
+                                <TextInput style={[InputStyle.large, InputStyle.default, InputStyle.white]} 
+                                                values={values.password} secureTextEntry={true} 
+                                                placeholder="Password" placeholderTextColor="#fff"
+                                                onChangeText={handleChange('password')} />
+                                <Text style={[LabelStyle.medium_label, LabelStyle.brand]} onPress={() => {
+                                    navigator.navigate('ForgotPassword');
+                                }}>Forgot password?</Text>
+                                <Text style={[LabelStyle.medium_label, LabelStyle.brand]} onPress={() => {
+                                    navigator.navigate('Signup');
+                                }}>Create account</Text>
+                                <TouchableOpacity style={ButtonStyle.large} onPress={handleSubmit}>
+                                    <Text style={ButtonStyle.large_text}>Sign in</Text>
+                                </TouchableOpacity>
+                            </View>
+                        )}
+                    </Formik>
                 </ScrollView>
             </ImageBackground>
         </Fragment>
--- packages/mobile/src/components/authentications/Signup.js
+++ packages/mobile/src/components/authentications/Signup.js
@@ -1,71 +1,137 @@
 import { Picker } from '@react-native-community/picker';
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { Input } from 'react-native-elements';
 import { ButtonStyle } from '../../styles/button_style';
 import { ContainerStyle } from '../../styles/container_style';
 import { LabelStyle } from '../../styles/label_style';
+import { Formik } from 'formik';
 
 function Signup(props) {
 
     const navigator = props.navigation;
+    let defaultValues = {
+        firstname: '',
+        lastname: '',
+        middlename: '',
+        mobile_no: '',
+        username: '',
+        password: '',
+        cpassword: '',
+        site: '',
+        org: ''
+    }
 
+    let sites = [
+        { label: 'Marirong', value: 'mar' },
+        { label: 'Umingan', value: 'umi' }
+    ]
+
+    let orgs = [
+        { label: 'LEWC', value: 'lewc' },
+        { label: 'BLGU', value: 'blgu' },
+        { label: 'MLGU', value: 'mlgu' },
+        { label: 'PLGU', value: 'PLGU' },
+        { label: 'PNP', value: 'pnp' },
+        { label: 'Public', value: 'pub' }
+    ]
+
+    const test = (value) => {
+        console.log(value)
+    }
     return (
         <Fragment>
             <ScrollView style={[ContainerStyle.content]}>
-                <View>
-                    <View style={{ alignItems: 'center', paddingTop: 20 }}>
-                        <Input placeholder="E.g. Juan"
-                            label="First name"
-                            containerStyle={{ width: '80%', borderColor: '#f5981c' }}
-                            inputStyle={{textAlign: 'center', padding: 0}} />
-                        <Input placeholder="E.g. Dela Cruz"
-                            label="Last name"
-                            containerStyle={{ width: '80%' }}
-                            inputStyle={{textAlign: 'center', padding: 0}} />
-                        <Input placeholder="E.g. Domgingo"
-                            label="Middle name"
-                            containerStyle={{ width: '80%' }}
-                            inputStyle={{textAlign: 'center', padding: 0}} />
-                        <Input placeholder="E.g. 091234567890"
-                            label="Mobile No."
-                            containerStyle={{ width: '80%' }}
-                            inputStyle={{borderBottomWidth: 2}}
-                            inputStyle={{textAlign: 'center', padding: 0}} />
-                        <View style={{ width: '77%', borderBottomWidth: 1, borderBottomColor: '#848e98', alignSelf: 'center'}}>
-                            <Text style={{fontWeight: 'bold', fontSize: 17, paddingLeft: 5, color: '#848e98'}}>Site Selection</Text>
-                            <Picker
-                                selectedValue={'mar'}
-                                mode='dropdown'>
-                                <Picker.Item label="Marirong" value="mar" />
-                                <Picker.Item label="Umingan" value="umi" />
-                            </Picker>
+                <Formik
+                    initialValues={defaultValues}
+                    onSubmit={values => console.log(values)}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values }) => (
+                        <View>
+                            <View style={{ alignItems: 'center', paddingTop: 20 }}>
+                                <Input placeholder="E.g. Juan"
+                                    label="First name"
+                                    value={values.firstname}
+                                    containerStyle={{ width: '80%', borderColor: '#f5981c' }}
+                                    inputStyle={{ textAlign: 'center', padding: 0 }}
+                                    onChangeText={handleChange('firstname')} />
+                                <Input placeholder="E.g. Dela Cruz"
+                                    label="Last name"
+                                    value={values.lastname}
+                                    containerStyle={{ width: '80%' }}
+                                    inputStyle={{ textAlign: 'center', padding: 0 }}
+                                    onChangeText={handleChange('lastname')} />
+                                <Input placeholder="E.g. Domgingo"
+                                    label="Middle name"
+                                    value={values.middlename}
+                                    containerStyle={{ width: '80%' }}
+                                    inputStyle={{ textAlign: 'center', padding: 0 }}
+                                    onChangeText={handleChange('middlename')} />
+                                <Input placeholder="E.g. 091234567890"
+                                    label="Mobile No."
+                                    value={values.mobile_no}
+                                    containerStyle={{ width: '80%' }}
+                                    inputStyle={{ textAlign: 'center', padding: 0 }}
+                                    onChangeText={handleChange('mobile_no')} />
+                                <Input placeholder="E.g. JuanDelaCruz"
+                                    label="Username"
+                                    value={values.username}
+                                    containerStyle={{ width: '80%' }}
+                                    inputStyle={{ textAlign: 'center', padding: 0 }}
+                                    onChangeText={handleChange('username')} />
+                                <Input
+                                    label="Password"
+                                    value={values.password}
+                                    secureTextEntry={true}
+                                    containerStyle={{ width: '80%' }}
+                                    inputStyle={{ textAlign: 'center', padding: 0 }} 
+                                    onChangeText={handleChange('password')}/>
+                                <Input
+                                    label="Confirm Password"
+                                    secureTextEntry={true}
+                                    value={values.cpassword}
+                                    containerStyle={{ width: '80%' }}
+                                    inputStyle={{ textAlign: 'center', padding: 0 }} 
+                                    onChangeText={handleChange('cpassword')}/>
+                                <View style={{ width: '77%', borderBottomWidth: 1, borderBottomColor: '#848e98', alignSelf: 'center' }}>
+                                    <Text style={{ fontWeight: 'bold', fontSize: 17, paddingLeft: 5, color: '#848e98' }}>Site Selection</Text>
+                                    <Picker
+                                        mode='dropdown'
+                                        selectedValue={values.site}
+                                        onValueChange={handleChange('site')}>
+                                        {
+                                            sites.map((site) => {
+                                                return <Picker.Item key={site.value} label={site.label} value={site.value} />
+                                            })
+                                        }
+                                    </Picker>
+                                </View>
+                                <View style={{ width: '77%', paddingTop: 25, borderBottomWidth: 1, borderBottomColor: '#848e98', alignSelf: 'center' }}>
+                                    <Text style={{ fontWeight: 'bold', fontSize: 17, paddingLeft: 5, color: '#848e98' }}>Organization</Text>
+                                    <Picker
+                                        mode='dropdown'
+                                        selectedValue={values.org}
+                                        onValueChange={handleChange('org')}>
+                                        {
+                                            orgs.map((org) => {
+                                                return <Picker.Item key={org.value} label={org.label} value={org.value} />
+                                            })
+                                        }
+                                    </Picker>
+                                </View>
+                            </View>
+                            <View style={{ alignItems: 'center', paddingTop: 25, paddingBottom: 25 }}>
+                                <Text style={[LabelStyle.small_label, { textAlign: 'center' }]}>* All fields are required when creating your CBEWS-L Account</Text>
+                                <Text style={[LabelStyle.small_label, { textAlign: 'center' }]}>* Please review your details before submitting</Text>
+                            </View>
+                            <View style={{ alignItems: 'center' }}>
+                                <TouchableOpacity style={ButtonStyle.medium} onPress={handleSubmit}>
+                                    <Text style={ButtonStyle.medium_text}>Submit</Text>
+                                </TouchableOpacity>
+                            </View>
                         </View>
-                        <View style={{ width: '77%',paddingTop: 25, borderBottomWidth: 1, borderBottomColor: '#848e98', alignSelf: 'center'}}>
-                            <Text style={{fontWeight: 'bold', fontSize: 17, paddingLeft: 5, color: '#848e98'}}>Organization</Text>
-                            <Picker
-                                selectedValue={'lewc'}
-                                mode='dropdown'>
-                                <Picker.Item label="LEWC" value="lewc" />
-                                <Picker.Item label="BLGU" value="blgu" />
-                                <Picker.Item label="MLGU" value="mlgu" />
-                                <Picker.Item label="PLGU" value="plgu" />
-                                <Picker.Item label="PDRRMO" value="pdrrmo" />
-                                <Picker.Item label="PNP" value="pnp" />
-                                <Picker.Item label="Public" value="pub" />
-                            </Picker>
-                        </View>
-                    </View>
-                    <View style={{ alignItems: 'center' , paddingTop: 25, paddingBottom: 25}}>
-                        <Text style={[LabelStyle.small_label, { textAlign: 'center' }]}>* All fields are required when creating your CBEWS-L Account</Text>
-                        <Text style={[LabelStyle.small_label, { textAlign: 'center' }]}>* Please review your details before submitting</Text>
-                    </View>
-                    <View style={{alignItems: 'center'}}>
-                        <TouchableOpacity style={ButtonStyle.medium}>
-                            <Text style={ButtonStyle.medium_text}>Submit</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
+                    )}
+                </Formik>
             </ScrollView>
         </Fragment>
     )
