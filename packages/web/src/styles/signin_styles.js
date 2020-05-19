c3e7ae8e909ea9ce941603c73806bbe0a8a5aeaa
John Louie Nepomuceno <jlouienepomuceno@gmail.com>
2020-05-19 22:10:55 +0800

    Added gitignore file to hide pycache folders and contents. Added missing style file

---
+++ packages/web/src/styles/signin_styles.js
@@ -0,0 +1,69 @@
+import { makeStyles } from "@material-ui/core/styles";
+// import images from "../assets/login_screen.png";
+import images from "../assets/login-bg-img.jpg";
+
+export default () =>
+    makeStyles((theme) => ({
+        root: {
+            height: "100vh",
+        },
+        image: {
+            backgroundImage: `url(${images})`,
+            backgroundRepeat: "no-repeat",
+            backgroundColor:
+                theme.palette.type === "light"
+                    ? theme.palette.grey[50]
+                    : theme.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+        },
+        paper: {
+            margin: theme.spacing(6, 4),
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+        },
+        paperBG: {
+            backgroundImage: `url(${images})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+        },
+        avatar: {
+            margin: theme.spacing(1),
+            backgroundColor: theme.palette.secondary.main,
+        },
+        form: {
+            width: "100%", // Fix IE 11 issue.
+            marginTop: theme.spacing(1),
+        },
+        submit: {
+            margin: theme.spacing(3, 0, 2),
+        },
+        xs_image: {
+            length: '80px',
+            width: '80px',
+        },
+
+        media: {
+            length: 200,
+            width: 200,
+        },
+
+        sign_in_header: {
+            textAlign: 'center',
+        },
+
+        logo_container: {
+            margin: '0%'
+        },
+
+                
+        formControl: {
+            // margin: theme.spacing(1),
+            minWidth: '100%',
+        },
+        selectEmpty: {
+            marginTop: theme.spacing(2),
+        },
+    }));
