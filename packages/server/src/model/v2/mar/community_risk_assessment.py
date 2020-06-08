3cd90bf4199a034ca0b04faf321eb66f6c07125f
John Geliberte <gelibertejohn@gmail.com>
2020-06-08 16:09:50 +0800

    + Finished Cav for MAR mobile

--- packages/server/src/model/v2/mar/community_risk_assessment.py
+++ packages/server/src/model/v2/mar/community_risk_assessment.py
@@ -4,48 +4,44 @@ from src.api.helpers import Helpers as h
 
 class CommunityRiskAssessment():
 
-    def create_cav(data):
-        schema = "cbewsl_mar_collections"
-        query = f"INSERT INTO capacity_and_vulnerability VALUES (0, '{data['resource']}', {data['quantity']}, '{data['status']}', " \
-                f"'{data['owner']}', '{data['in_charge']}', '{data['updater']}', '{data['datetime']}', {data['user_id']}, '{data['last_ts']}')"
-        try:
-            cav_id = DB.db_modify(query, schema, True)
-        except Exception as err:
-            raise(err)
-        return cav_id
+	def create_cav(data):
+		schema = "cbewsl_mar_collections"
+		query = f"INSERT INTO capacity_and_vulnerability VALUES (0, '{data['resource']}', {data['quantity']}, '{data['status']}', " \
+				f"'{data['owner']}', '{data['in_charge']}', '{data['updater']}', '{data['datetime']}', {data['user_id']}, '{str(dt.today())}')"
+		try:
+			cav_id = DB.db_modify(query, schema, True)
+		except Exception as err:
+			raise(err)
+		return cav_id
 
-    def fetch_cav(cav_id):
-        if cav_id == "all":
-            query = 'SELECT * FROM capacity_and_vulnerability'
-        else:
-            query = f'SELECT * FROM capacity_and_vulnerability WHERE cav_id = "{cav_id}"'
-        schema = "cbewsl_mar_collections"
-        result = DB.db_read(query, schema)
-        return result
-    
-    def update_cav(data):
-        query = "UPDATE capacity_and_vulnerability SET "
+	def fetch_cav(cav_id):
+		if cav_id == "all":
+			query = 'SELECT * FROM capacity_and_vulnerability'
+		else:
+			query = f'SELECT * FROM capacity_and_vulnerability WHERE cav_id = "{cav_id}"'
+		schema = "cbewsl_mar_collections"
+		result = DB.db_read(query, schema)
+		return result
+	
+	def update_cav(data):
+		query = "UPDATE capacity_and_vulnerability SET"
+		counter = 0
+		for x in data:
+			key = list(x)[0]
+			if 'cav_id' == key:
+				query = f"{query}, last_ts = '{str(dt.today())}' WHERE cav_id = '{x[key]}'"
+			else:
+				if counter == 0:
+					query += f" {key} = '{x[key]}'"
+				else:
+					query += f", {key} = '{x[key]}'"
+			counter += 1
+		ret_val = DB.db_modify(query,'cbewsl_mar_collections', True)
+		return ret_val
 
-        end = ""
-        counter = 0
-        for key, value in data.items():
-            if key == "cav_id":
-                end = f", last_ts='{dt.now()}' WHERE cav_id={value}"
-            else:
-                if counter == 0:
-                    query += f" {key} = '{value}'"
-                else:
-                    query += f", {key} = '{value}'"
-            counter += 1
-
-        query = f"{query}{end}"
-        schema = "cbewsl_mar_collections"
-        result = DB.db_modify(query, schema, True)
-
-        return result
-
-    def delete_cav(cav_id):
-        query = f'DELETE FROM capacity_and_vulnerability WHERE cav_id = { cav_id }'
-        schema = "cbewsl_mar_collections"
-        result = DB.db_modify(query, schema, True)
-        return result
+	def delete_cav(data):
+		query = f"DELETE FROM capacity_and_vulnerability WHERE cav_id = { data['cav_id'] }"
+		schema = "cbewsl_mar_collections"
+		print(query)
+		result = DB.db_modify(query, schema, True)
+		return result
