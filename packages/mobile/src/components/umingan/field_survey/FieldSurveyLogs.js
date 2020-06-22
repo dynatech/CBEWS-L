d81f0845597e7e7c6d123d48445c25deea7bdeca
John Geliberte <gelibertejohn@gmail.com>
2020-06-23 00:40:12 +0800

    + Added field survey navigation for umingan mobile app

---
+++ packages/mobile/src/components/umingan/field_survey/FieldSurveyLogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+function FieldSurveyLogs() {
+    return(
+        <View>
+            <Text>Field Survey Logs</Text>
+        </View>
+    )
+}
+
+export default FieldSurveyLogs
---
+++ packages/mobile/src/components/umingan/field_survey/ReportSummary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+function ReportSummary() {
+    return(
+        <View>
+            <Text>Report Summary</Text>
+        </View>
+    )
+}
+
+export default ReportSummary
