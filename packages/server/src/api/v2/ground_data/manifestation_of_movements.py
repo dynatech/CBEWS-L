61f9a07e14933b00f47f49a15159d6af7b906eda
jlnepomuceno <jlouienepomuceno@gmail.com>
2020-09-28 01:50:44 +0800

    Simplified moms APIs

--- packages/server/src/api/v2/ground_data/manifestation_of_movements.py
+++ packages/server/src/api/v2/ground_data/manifestation_of_movements.py
@@ -14,60 +14,23 @@ def modify_ts_format(entry):
     return entry
 
 
-@MANIFESTATION_OF_MOVEMENTS_BLUEPRINT.route("/get/ground_data/<site_code>/moms", methods=["GET"])
-def fetch_moms(site_code):
-    try:
-        site_id = APP_CONFIG["site_ids"][site_code]
-        moms_list = GroundData.fetch_moms(site_id)
-        moms_list = list(map(modify_ts_format, moms_list))
-        moms = {
-            "status": True,
-            "data": moms_list
-        }
-    except Exception as err:
-        moms = {
-            "status": False,
-            "message": f"Failed to fetch {site_code.upper()} moms data. Error: {err}"
-        }
-    return jsonify(moms)
-
+##############################
+# MONITORING_MOMS (MOMS REPORTS/OBSERVATIONS)
+##############################
 
 @MANIFESTATION_OF_MOVEMENTS_BLUEPRINT.route("/add/ground_data/mar/moms", methods=["POST"])
 def add():
     try:
         data = request.get_json()
         ts = data["ts"]
-        # feature_id = data["feature_id"]
-        feature_type = data["feature_type"]
-        feature_name = data["feature_name"]
-
-        reporter = data["reporter"]
-        location = data["location"]
+        instance_id = data["instance_id"]
         remarks = data["remarks"]
-        site_id = data["site_id"]
-        user_id = data["user_id"]
+        reporter_id = data["reporter_id"]
         alert_level = data["alert_level"]
-        try:
-            description = data["description"]
-        except KeyError:
-            description = None
-
-        feature_type_temp = GroundData.fetch_feature_id(feature_type, site_id)
-        if feature_type_temp:
-            feature_id = feature_type_temp[0]["feature_id"]
-        else:
-            feature_id = GroundData.insert_moms_feature(feature_type=feature_type, site_id=site_id, description=description)
-
-        moms_instance = GroundData.fetch_feature_name(feature_id, feature_name, site_id)
-        if moms_instance:
-            instance_id = moms_instance[0]["instance_id"]
-        else:
-            instance_id = GroundData.insert_moms_instance(site_id, feature_id, feature_name, 
-                                                            location, reporter)
 
         moms_id = GroundData.insert_moms_record(
             instance_id=instance_id, ts=ts, remarks=remarks,
-            reporter_id=user_id, alert_level=alert_level
+            reporter_id=reporter_id, alert_level=alert_level
         )
 
         trigger_sym_id = AlertGen.get_operational_trigger_symbol(
@@ -118,68 +81,45 @@ def add():
     return jsonify(moms)
 
 
-@MANIFESTATION_OF_MOVEMENTS_BLUEPRINT.route("/update/ground_data/moms", methods=["POST"])
-def update():
+@MANIFESTATION_OF_MOVEMENTS_BLUEPRINT.route("/get/ground_data/<site_code>/moms", methods=["GET"])
+def fetch_moms(site_code):
     try:
-        data = request.get_json()
+        site_id = APP_CONFIG["site_ids"][site_code]
+        moms_list = GroundData.fetch_moms(site_id)
+        moms_list = list(map(modify_ts_format, moms_list))
+        moms = {
+            "status": True,
+            "data": moms_list
+        }
+    except Exception as err:
+        moms = {
+            "status": False,
+            "message": f"Failed to fetch {site_code.upper()} moms data. Error: {err}"
+        }
+    return jsonify(moms)
+
+
+@MANIFESTATION_OF_MOVEMENTS_BLUEPRINT.route("/update/ground_data/moms", methods=["POST"])
+def update_monitoring_moms():
+    try:
+        data_list = request.get_json()
         
-        moms_id = data["moms_id"]
-        remarks = data["remarks"]
-        ts = data["ts"]
-        feature_type = data["feature_type"]
-        feature_name = data["feature_name"]
-        reporter = data["reporter"]
-        location = data["location"]
-        site_id = data["site_id"]
-        user_id = data["user_id"]
-        alert_level = data["alert_level"]
-
-        try:
-            description = data["description"]
-        except KeyError:
-            description = None
-
-        feature_type_temp = GroundData.fetch_feature_id(feature_type, site_id)
-        if feature_type_temp:
-            feature_id = feature_type_temp[0]["feature_id"]
-        else:
-            feature_id = GroundData.insert_moms_feature(feature_type=feature_type, site_id=site_id, description=description)
-
-        moms_instance = GroundData.fetch_feature_name(feature_id, feature_name, site_id)
-        if moms_instance:
-            instance_id = moms_instance[0]["instance_id"]
-        else:
-            instance_id = GroundData.insert_moms_instance(site_id, feature_id, feature_name, 
-                                                            location, reporter)
-
-        if instance_id:
-            moms_monitoring_update = GroundData.update_monitoring_moms(moms_id, remarks)
-            if moms_monitoring_update['status'] == True:
-                moms = {
-                    "status": True,
-                    "message": "Successfully updated Manifestation of Movements."
-                }
-            else:
-                moms = {
-                    "status": False,
-                    "message": "Failed to update monitoring_moms table (remarks) data."
-                }
-        else:
-            moms = {
-                "status": False,
-                "message": "Failed to update moms instance table (location, reporter) data."
-            }
+        moms_monitoring_update = GroundData.update_monitoring_moms(data_list)
+        moms = {
+            "status": True,
+            "message": "Successfully updated Manifestation of Movements."
+        }
     except Exception as err:
         raise(err)
         moms = {
             "status": False,
-            "message": f"Failed to fetch moms data. Error: {err}"
+            "message": "Failed to update monitoring_moms table (remarks) data."
         }
     return jsonify(moms)
 
 
 @MANIFESTATION_OF_MOVEMENTS_BLUEPRINT.route("/delete/ground_data/moms", methods=["delete"])
-def delete():
+def delete_moms():
     try:
         moms = request.get_json()
         moms_delete = GroundData.delete_moms_observation(moms['moms_id'])
@@ -202,30 +142,82 @@ def delete():
     return jsonify(moms)
 
 
+##############################
+# MOMS INSTANCES (NAMES)
+##############################
+
+@MANIFESTATION_OF_MOVEMENTS_BLUEPRINT.route("/add/ground_data/moms/instance", methods=["POST"])
+def insert_moms_instance():
+    try:
+        data = request.get_json()
+        result = GroundData.insert_moms_instance(data)
+        H.var_checker("result", result)
+        return_value = {
+            "status": True,
+            "instance_id": result,
+            "message": "MOMS Instance successfully updated!"
+        }
+    except Exception as err:
+        return_value = {
+            "status": False,
+            "message": f"Failed to update moms instance -> {err}"
+        }
+    return jsonify(return_value)
+
+
 @MANIFESTATION_OF_MOVEMENTS_BLUEPRINT.route("/get/ground_data/moms/feature/<feature_id>/<site_id>", methods=["GET"])
-def fetch_feature(feature_id, site_id):
+def fetch_moms_instances(feature_id, site_id):
     try:
         name_container = []
-        feature_names = GroundData.fetch_feature_name(feature_id, None, site_id)
-        for name in feature_names:
-            instance_id = name["instance_id"]
-            feature_name = name["feature_name"]
-            location = name["location"]
-            reporter = name["reporter"]
-
-            name_container.append({
-                "instance_id": instance_id,
-                "feature_name": feature_name,
-                "location": location,
-                "reporter": reporter
-            })
-        moms = {"status": True, "data": name_container}
+        moms_instances = GroundData.fetch_feature_name(feature_id, None, site_id)
+        moms = {"status": True, "data": moms_instances}
     except Exception as err:
         moms = {
             "status": False,
             "message": f"Failed to fetch moms data. Error: {err}"
         }
     return jsonify(moms)
+
+
+@MANIFESTATION_OF_MOVEMENTS_BLUEPRINT.route("/update/ground_data/moms/instance", methods=["POST"])
+def update_moms_instance():
+    try:
+        data_list = request.get_json()
+        GroundData.update_moms_instance(data_list)
+        return_value = {
+            "status": True,
+            "message": "MOMS Instance successfully updated!"
+        }
+    except Exception as err:
+        print(err)
+        return_value = {
+            "status": False,
+            "message": f"Failed to update moms instance -> {err}"
+        }
+    return jsonify(return_value)
+
+
+##############################
+# MOMS FEATURES (TYPES)
+##############################
+
+@MANIFESTATION_OF_MOVEMENTS_BLUEPRINT.route("/add/ground_data/moms/feature/types", methods=["POST"])
+def insert_moms_feature():
+    try:
+        data = request.get_json()
+        result = GroundData.insert_moms_feature(data)
+        H.var_checker("result", result)
+        return_value = {
+            "status": True,
+            "instance_id": result,
+            "message": "MOMS feature successfully updated!"
+        }
+    except Exception as err:
+        return_value = {
+            "status": False,
+            "message": f"Failed to update moms feature -> {err}"
+        }
+    return jsonify(return_value)
 
 
 @MANIFESTATION_OF_MOVEMENTS_BLUEPRINT.route("/get/ground_data/moms/feature/types", methods=["GET"])
@@ -243,3 +235,21 @@ def fetch_moms_feature_types():
             "message": f"Failed to fetch moms data. Error: {err}"
         }
     return jsonify(response)
+
+
+@MANIFESTATION_OF_MOVEMENTS_BLUEPRINT.route("/update/ground_data/moms/feature/types", methods=["POST"])
+def update_moms_feature():
+    try:
+        data_list = request.get_json()
+        GroundData.update_moms_feature(data_list)
+        return_value = {
+            "status": True,
+            "message": "MOMS Feature successfully updated!"
+        }
+    except Exception as err:
+        print(err)
+        return_value = {
+            "status": False,
+            "message": f"Failed to update moms feature -> {err}"
+        }
+    return jsonify(return_value)
