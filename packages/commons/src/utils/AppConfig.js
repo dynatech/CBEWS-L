3467d2c2858e85f679e01475613ea8c8b21afaf8
jlnepomuceno <jlouienepomuceno@gmail.com>
2020-12-01 07:40:49 +0800

    Changed web ip

--- packages/commons/src/utils/AppConfig.js
+++ packages/commons/src/utils/AppConfig.js
@@ -5,7 +5,7 @@ const web_ip = "https://dynaslope.phivolcs.dost.gov.ph"
 const AppConfig = {
     HOSTNAME: `${web_ip}:5000`,
     HOST_DIR: `${web_ip}/`,
-    UMINGAN_DIR: `${AppDir}\\Work\\CBEWS-L\\packages\\commons\\src\\client-cbewsl\\UMINGAN`,
+    UMINGAN_DIR: `${AppDir}/CBEWS-L/packages/commons/src/client-cbewsl/UMINGAN`,
     MARIRONG_DIR: `${AppDir}/CBEWS-L/packages/commons/src/client-cbewsl/MARIRONG`
 };
 
