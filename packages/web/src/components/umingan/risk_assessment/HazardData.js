a60bb388029f05167a69526a4385ff3ed3ba76b6
pulidongz <paogravoso@gmail.com>
2021-04-16 17:29:00 +0800

    UMI: Added table batch delete for hazard data

--- packages/web/src/components/umingan/risk_assessment/HazardData.js
+++ packages/web/src/components/umingan/risk_assessment/HazardData.js
@@ -46,8 +46,17 @@ export default function HazardData(props) {
     const [defaultIntValues, setDefaultIntValues] = useState({});
 
     const options = {
+        filter: true,
+        selectableRows: "multiple",
+        selectableRowsOnClick: true,
         filterType: "checkbox",
+        responsive: "vertical",
+        onRowsDelete: (rowsDeleted) => {
+            const idsToDelete = rowsDeleted.data.map (item => item.dataIndex)
+            handleMuiTableBatchDelete(idsToDelete.sort());
+          }
     };
+
     const columns = [
         { name: "hazard", label: "Hazard" },
         { name: "speed_of_onset", label: "Speed of Onset" },
@@ -98,6 +107,48 @@ export default function HazardData(props) {
     const handleDelete = (data) => {
         setSelectedData(data);
         handleOpenDelete();
+    };
+
+    const handleMuiTableBatchDelete = (data) => {
+        var obj_to_delete = [];
+        var toDelete = [];
+        
+        var temp_data = tableData.map((e, key)=>({
+            "row_id": key,
+            "id": e.id,
+        }))
+
+        for (let index = 0; index < data.length; index++) {
+            toDelete = temp_data.filter(e => e.row_id === data[index]);
+
+            if (toDelete.length) {
+                obj_to_delete.push(toDelete);
+            }
+        }
+        obj_to_delete = obj_to_delete.flat()
+        obj_to_delete.forEach(e => deleteTableEntry(e.id));
+        initTable();
+        resetState();
+    };
+
+    const deleteTableEntry = async (data) => {
+
+        const input = {
+            id: data,
+        };
+        
+        const response = await UmiRiskManagement.DeleteHazardData(input);
+        if (response.status === true) {
+            setOpenNotif(true);
+            setNotifStatus("success");
+            setNotifText("Successfully deleted hazard data.");
+        } else {
+            setOpenNotif(true);
+            setNotifStatus("error");
+            setNotifText(
+                "Failed to delete hazard data. Please contact the developers or file a bug report",
+            );
+        }
     };
 
     const handleOpenDelete = () => {
@@ -205,6 +256,7 @@ export default function HazardData(props) {
                         data={{
                             columns: columns,
                             rows: tableData,
+                            options:options,
                         }}
                         handlers={{
                             handleAdd,
