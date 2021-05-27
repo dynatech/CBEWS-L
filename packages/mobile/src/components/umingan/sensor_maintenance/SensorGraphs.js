608c741f218b592e5c040a5a9701bd9c5fee8f84
John Louie Nepomuceno <jlouienepomuceno@gmail.com>
2021-05-27 11:28:19 +0800

    Updated sensor graphs api. working requests and tweaks to user interface

--- packages/mobile/src/components/umingan/sensor_maintenance/SensorGraphs.js
+++ packages/mobile/src/components/umingan/sensor_maintenance/SensorGraphs.js
@@ -5,6 +5,8 @@ import { LabelStyle } from '../../../styles/label_style';
 import { ButtonStyle } from '../../../styles/button_style';
 import { ColPos, Displacement, Velocity } from '../../utils/SubsurfaceGraph';
 import { RainfallGraph } from '../../utils/RainfallGraph';
+import MobileCaching from '../../../utils/MobileCaching';
+import NetworkUtils from '../../../utils/NetworkUtils';
 
 import { UmiDataAnalysis } from "@dynaslope/commons";
 
@@ -13,31 +15,73 @@ function SensorGraphs() {
     const temp = require('../../../assets/blcsb.json');
     const temp_rain = require('../../../assets/rain_data_mar.json');
     const [rainfallData, setRainfallData] = useState([]);
+    const [latestRainTs, setLatestRainTs] = useState(null);
+    const [subsurfaceData, setSubsurfaceData] = useState([]);
+    const [latestSubTs, setLatestSubTs] = useState(null);
 
     useEffect(()=> {
-        console.log("temp_rain", temp_rain);
-        initRainfall();
+        // console.log("temp_rain", temp_rain);
+        setTimeout( async () => {
+            const isConnected = await NetworkUtils.isNetworkAvailable()
+            if (isConnected != true) {
+                MobileCaching.getItem('UmiSensorGraphsRainfallData').then(response => {
+                    console.log("Getting data from UmiSensorGraphsRainfallData cache")
+                    setRainfallData(response);
+                });
+                MobileCaching.getItem('UmiSensorGraphsSubsurfaceData').then(response => {
+                    console.log("Getting data from UmiSensorGraphsSubsurfaceData cache")
+                    init(response);
+                    setSubsurfaceData(response);
+                });
+            } else {
+                initRainfall();
+                initSubsurface();
+            }
+        }, 100);
     }, []);
 
     const initRainfall = async () => {
         const response = await UmiDataAnalysis.GetRainfallPlotData();
         console.log("response", response)
-        if (response.status === true) {
+        if (response.status == true) {
             let rainfall_data = response.data[0];
-            console.log("rainfall_data", rainfall_data)
-            setRainfallData(rainfall_data);
+            console.log("temp_rain", temp_rain);
+            console.log("rainfall_data", rainfall_data);
+            setLatestRainTs(rainfallData.ts_end);
+            setRainfallData(response.data);
+            MobileCaching.setItem('UmiSensorGraphsRainfallData', response.data);
         } else {
-            console.error(response.message);
+            ToastAndroid.showWithGravity(response.message, ToastAndroid.LONG, ToastAndroid.CENTER)
         }
-    }
+    };
+
+    const initSubsurface = async () => {
+        const response = await UmiDataAnalysis.GetSubsurfacePlotData();
+        console.log("response", response)
+        if (response.status == true) {
+            let subsurface_data = response.data[0];
+            console.log("temp subs", temp);
+            console.log("subsurface_data", subsurface_data)
+            setLatestSubTs(null);
+            setSubsurfaceData(response.data);
+            MobileCaching.setItem('UmiSensorGraphsSubsurfaceData', response.data);
+        } else {
+            ToastAndroid.showWithGravity(response.message, ToastAndroid.LONG, ToastAndroid.CENTER)
+        }
+    };
 
     return(
         <ScrollView>
             <View style={ContainerStyle.content}>
                 <Text style={[LabelStyle.large_label, LabelStyle.brand]}>Rainfall Graphs</Text>
-                <Text style={[LabelStyle.small_label, LabelStyle.brand]}>Latest data as of YYYY-MM-DD HH:mm:ss</Text>
                 {
-                    temp_rain.data.map((element)=> {
+                    latestRainTs !== null ?
+                        <Text style={[LabelStyle.small_label, LabelStyle.brand]}>Latest data as of {latestRainTs}</Text>
+                        :
+                        <Text style={[LabelStyle.small_label, LabelStyle.brand]}>Loading rainfall data...</Text>
+                }
+                {
+                    rainfallData.map((element)=> {
                         let ret_val = [];
                         element.plot.forEach(row => {
                             ret_val.push(
@@ -52,9 +96,14 @@ function SensorGraphs() {
             </View>
             <View style={ContainerStyle.content}>
                 <Text style={[LabelStyle.large_label, LabelStyle.brand]}>Subsurface Graphs</Text>
-                <Text style={[LabelStyle.small_label, LabelStyle.brand]}>Latest data as of YYYY-MM-DD HH:mm:ss</Text>
                 {
-                    temp.data[0].map((element)=> {
+                    latestSubTs !== null ? 
+                        <Text style={[LabelStyle.small_label, LabelStyle.brand]}>Latest data as of YYYY-MM-DD HH:mm:ss</Text> 
+                        : 
+                        <Text style={[LabelStyle.small_label, LabelStyle.brand]}>Loading subsurface data...</Text>
+                }
+                {
+                    subsurfaceData.map((element)=> {
                         let ret_val = [];
                         if (element.type == "column_position") {
                             element.data.data.forEach(row => {
