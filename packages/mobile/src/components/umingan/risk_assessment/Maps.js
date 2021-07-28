2d1a377516a88b14cf0cf6d2413cc387659ee47a
pulidongz <paogravoso@gmail.com>
2021-07-28 19:30:50 +0800

    UMI: Maps - show map image, zoom

--- packages/mobile/src/components/umingan/risk_assessment/Maps.js
+++ packages/mobile/src/components/umingan/risk_assessment/Maps.js
@@ -1,50 +1,93 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Modal, TouchableHighlight, Image } from 'react-native';
+import { View, Text, Modal, ScrollView, TouchableOpacity, TouchableHighlight, Image, RefreshControl, SafeAreaView } from 'react-native';
 import ImageViewer from 'react-native-image-zoom-viewer';
 import { ContainerStyle } from '../../../styles/container_style';
+import { ImageStyle } from '../../../styles/image_style';
 import { LabelStyle } from '../../../styles/label_style';
 import { ButtonStyle } from '../../../styles/button_style';
 import { InputStyle } from '../../../styles/input_style';
+import { UmiRiskManagement, AppConfig } from '@dynaslope/commons';
+import MobileCaching from '../../../utils/MobileCaching';
+import NetworkUtils from '../../../utils/NetworkUtils';
+import { useIsFocused } from '@react-navigation/native';
+import moment from 'moment';
 
 function Maps() {
+	const [mapView, setMapView] = useState(false);
+	const [mapList, setMapList] = useState([]);
+	const [mapTS, setMapTS] = useState(null);
+	const [refreshing, setRefreshing] = useState(false);
+	const isFocused = useIsFocused();
 
-    const [mapView, setMapView] = useState(false);
-    const [mapList, setMapList] = useState([])
+	useEffect(() => {
+			if(isFocused){
+				initMaps();
+			}
+	}, [isFocused])
 
-    useEffect(() => {
-        // Set fetching of maps
-    }, [])
+	const initMaps = async () => {
+		const response = await UmiRiskManagement.GetHazardMaps();
+		if (response.status) {
+			if (response.data.length > 0) {
+				setMapList({uri: `${AppConfig.HOST_DIR}:5001/src/client-cbewsl/UMINGAN/MAPS/${response.data[0].filename}`});
+				setMapTS(response.data[0].ts);
+			}
+		}
+	};
 
-    const showMaps = () => {
+	// Refresh Maps on pull down
+	const onRefresh = React.useCallback(() => {
+		setRefreshing(true);
+		initMaps().then(() => setRefreshing(false));
+	}, []);
 
-    }
+	const showMaps = () => {
+		setMapView(true);
+	}
 
-    const hideMaps = () => {
+	const hideMaps = () => {
+		setMapView(false);
+	}
+    
+	return (
+		<SafeAreaView>
+			<ScrollView refreshControl={
+				<RefreshControl
+						refreshing={refreshing}
+						onRefresh={onRefresh}
+				/>
+				}
+			>
+				<View style={ContainerStyle.content}>
+						<Text style={[LabelStyle.large_label, LabelStyle.brand]}>Latest Hazard Map for Umingan, Alimodian, Iloilo as of {moment(mapTS).format("MMMM D, YYYY h:mm A")}</Text>
+						{mapList.length == 0 ?
+								<Text style={[LabelStyle.large_label, LabelStyle.brand]} > No Hazard map available.</Text>
+								:
+								<TouchableHighlight onPress={() => showMaps()}>
+										<Image
+												style={ImageStyle.hazard_maps}
+												source={mapList}
+												// source={{uri:'http://192.168.0.150:5001/src/client-cbewsl/UMINGAN/MAPS/hazard_map_01.jpg'}}
+												resizeMode="center" />
+								</TouchableHighlight>
+						}
+						<Text style={[LabelStyle.small_label, LabelStyle.brand]}>* Click image to enlarge.</Text>
+						<Modal visible={mapView}
+							transparent={true}>
+							<ImageViewer imageUrls={[{url:mapList.uri}]} enableSwipeDown={true} onSwipeDown={() => { hideMaps() }} />
+						</Modal>
+						<View style={{ paddingTop: '10%', alignItems: 'center' }}>
+						</View>
+				</View>
+				<View style={{ flex: 1, alignItems: 'center', padding: 10 }}>
+					<TouchableOpacity style={ButtonStyle.medium} onPress={() => { initMaps() }}>
+						<Text style={ButtonStyle.large_text}>Map +</Text>
+					</TouchableOpacity>
+			</View>
 
-    }
-
-    return (
-        <View style={ContainerStyle.content}>
-            <Text style={[LabelStyle.large_label, LabelStyle.brand]}>Latest Hazard Map for Umingan, Alimodian, Iloilo as of (last uploaded ts)</Text>
-            {mapList.length == 0 ?
-                <Text style={[LabelStyle.large_label, LabelStyle.brand]} > No Hazzard map available.</Text>
-                :
-                <TouchableHighlight onPress={() => this.showMapFunction()}>
-                    <Image
-                        style={ImageStyle.hazard_maps}
-                        source={mapList[0]}
-                        resizeMode="center" />
-                </TouchableHighlight>
-            }
-            <Text style={[LabelStyle.small_label, LabelStyle.brand]}>* Click image to enlarge.</Text>
-            <Modal visible={mapView}
-                transparent={true}>
-                <ImageViewer imageUrls={mapList} enableSwipeDown={true} onSwipeDown={() => { hideMaps() }} />
-            </Modal>
-            <View style={{ paddingTop: '10%', alignItems: 'center' }}>
-            </View>
-        </View>
-    )
+			</ScrollView>
+		</SafeAreaView>
+	)
 }
 
 export default Maps;
