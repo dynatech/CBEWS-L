b3853aaee760b9104800a362b07d4c83778e9166
pulidongz <paogravoso@gmail.com>
2021-08-02 00:19:44 +0800

    Mar hazard map upload

--- packages/mobile/src/components/marirong/community_risk_assessment/HazardMapping.js
+++ packages/mobile/src/components/marirong/community_risk_assessment/HazardMapping.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Modal, ScrollView, TouchableOpacity, TouchableHighlight, Image, RefreshControl, SafeAreaView } from 'react-native';
+import React, { useState, useEffect, Fragment } from 'react';
+import { View, Text, Modal, ScrollView, TouchableOpacity, TouchableHighlight, Image, ToastAndroid, RefreshControl, SafeAreaView } from 'react-native';
 import ImageViewer from 'react-native-image-zoom-viewer';
 import { ContainerStyle } from '../../../styles/container_style';
 import { ImageStyle } from '../../../styles/image_style';
@@ -7,21 +7,23 @@ import { LabelStyle } from '../../../styles/label_style';
 import { ButtonStyle } from '../../../styles/button_style';
 import { MarCommunityRiskAssessment, AppConfig } from '@dynaslope/commons';
 import { useIsFocused } from '@react-navigation/native';
+import DocumentPicker from 'react-native-document-picker';
 import moment from 'moment';
 
 function HazardMapping() {
-
-    const [mapView, setMapView] = useState(false);
-    const [mapList, setMapList] = useState([]);
-    const [mapTS, setMapTS] = useState(null);
+	const [mapView, setMapView] = useState(false);
+	const [isDisabled, setIsDisabled] = useState(false);
+	const [mapList, setMapList] = useState([]);
+	const [mapTS, setMapTS] = useState(null);
+	const [singleFile, setSingleFile] = useState(null);
 	const [refreshing, setRefreshing] = useState(false);
 	const isFocused = useIsFocused();
 
     useEffect(() => {
-        if(isFocused){
-            initMaps();
-        }
-}, [isFocused])
+			if(isFocused){
+				initMaps();
+			}
+	}, [isFocused])
 
     const initMaps = async () => {
 		const response = await MarCommunityRiskAssessment.GetHazardMaps();
@@ -33,55 +35,111 @@ function HazardMapping() {
 		}
 	};
 
-    // Refresh Maps on pull down
+  // Refresh Maps on pull down
 	const onRefresh = React.useCallback(() => {
 		setRefreshing(true);
 		initMaps().then(() => setRefreshing(false));
 	}, []);
 
-    const showMaps = () => {
+  const showMaps = () => {
 		setMapView(true);
 	}
-
 	const hideMaps = () => {
 		setMapView(false);
 	}
-    
-    return (
-    <SafeAreaView>
-        <ScrollView refreshControl={
-            <RefreshControl
-                    refreshing={refreshing}
-                    onRefresh={onRefresh}
-            />
-            }
-        >
-            <View style={ContainerStyle.content}>
-            <Text style={[LabelStyle.large_label, LabelStyle.brand]}>Latest Hazard Map for Marirong, Leon, Iloilo as of {moment(mapTS).format("MMMM D, YYYY h:mm A")}</Text>
-            {mapList.length == 0 ?
-                <Text style={[LabelStyle.large_label, LabelStyle.brand]} > No Hazard map available.</Text>
-                :
-                <TouchableHighlight onPress={() => showMaps()}>
-                    <Image
-                        style={ImageStyle.hazard_maps}
-                        source={mapList}
-                        resizeMode="center" />
-                </TouchableHighlight>
-            }
-            <Text style={[LabelStyle.small_label, LabelStyle.brand]}>* Click image to enlarge.</Text>
-            <Modal visible={mapView}
-                transparent={true}>
-                <ImageViewer imageUrls={[{url:mapList.uri}]} enableSwipeDown={true} onSwipeDown={() => { hideMaps() }} />
-            </Modal>
-            <View style={{ flex: 1, alignItems: 'center', padding: 10 }}>
-                <TouchableOpacity style={ButtonStyle.medium} onPress={() => { initMaps() }}>
-                    <Text style={ButtonStyle.large_text}>Map +</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    	</ScrollView>
-	</SafeAreaView>
-    )
+	const disableUploadButton = () => {
+		setIsDisabled(true);
+	}
+	const enableUploadButton = () => {
+		setIsDisabled(false);
+	}
+
+	const handleUpload = async () => {
+		disableUploadButton();
+		const fileToUpload = singleFile;
+		const data = new FormData();
+		data.append('file', fileToUpload);
+		const response = await MarCommunityRiskAssessment.UploadHazardMaps(data);
+		if (response.status === true) {
+			ToastAndroid.showWithGravity(response.message, ToastAndroid.SHORT, ToastAndroid.CENTER)
+		} else {
+			ToastAndroid.showWithGravity(response.message, ToastAndroid.SHORT, ToastAndroid.CENTER)
+		}
+		setSingleFile(null);
+		initMaps();
+		enableUploadButton();
+	}
+
+	const selectFile = async () => {
+    try {
+      const res = await DocumentPicker.pick({
+        // Provide which type of file you want user to pick
+        type: [DocumentPicker.types.images],
+      });
+      setSingleFile(res);
+    } catch (err) {
+      setSingleFile(null);
+      // Handling any exception (If any)
+      if (DocumentPicker.isCancel(err)) {
+        // If user canceled the document selection
+        setSingleFile(null);
+      } else {
+        // For Unknown Error
+        alert('Unknown Error: ' + JSON.stringify(err));
+        throw err;
+      }
+    }
+  };
+
+	return (
+	<SafeAreaView>
+			<ScrollView refreshControl={
+					<RefreshControl
+									refreshing={refreshing}
+									onRefresh={onRefresh}
+					/>
+					}
+			>
+				<View style={ContainerStyle.content}>
+					<Text style={[LabelStyle.large_label, LabelStyle.brand]}>Latest Hazard Map for Marirong, Leon, Iloilo as of {moment(mapTS).format("MMMM D, YYYY h:mm A")}</Text>
+					{mapList.length == 0 ?
+						<Text style={[LabelStyle.large_label, LabelStyle.brand]} > No Hazard map available.</Text>
+						:
+						<TouchableHighlight onPress={showMaps}>
+							<Image
+							style={ImageStyle.hazard_maps}
+							source={mapList}
+							resizeMode="center"
+							/>
+						</TouchableHighlight>
+					}
+					<Text style={[LabelStyle.small_label, LabelStyle.brand]}>* Click image to enlarge.</Text>
+					<Modal visible={mapView}
+							transparent={true}>
+							<ImageViewer imageUrls={[{url:mapList.uri}]} enableSwipeDown={true} onSwipeDown={() => { hideMaps() }} />
+					</Modal>
+			</View>
+			<View style={{ flex: 1, alignItems: 'center', padding: 10 }}>
+				{/* Show Map+ when singleFile is empty, else show Upload */}
+				{singleFile == null ? (
+					<TouchableOpacity style={ButtonStyle.medium} onPress={selectFile}>
+						<Text style={ButtonStyle.large_text}>Map +</Text>
+					</TouchableOpacity>
+				):(
+					<Fragment>
+						<Text>
+							File Name: {singleFile.name ? singleFile.name : ''}
+							{'\n'}
+						</Text>
+						<TouchableOpacity style={ButtonStyle.medium} onPress={handleUpload} disabled={isDisabled}>
+							<Text style={ButtonStyle.large_text}>Upload Map</Text>
+						</TouchableOpacity>
+					</Fragment>
+				)}
+			</View>
+		</ScrollView>
+</SafeAreaView>
+	)
 }
 
 export default HazardMapping;
